5efdf09db57a038b9ebaba91b3463e370d2315fb
Yash Singh <138406826+yash-singhh@users.noreply.github.com>
2023-12-29 13:29:17 +0530

    Add files via upload

---
+++ whasAPP.py
@@ -0,0 +1,18 @@
+"// how to connect to mongodb in nodejs ? "
+
+
+# pip install pywhatkit
+
+# import pywhatkit
+# pywhatkit.sendwhatmsg('+91 9833132230','hey professional keep going... hard work pays off:)',22,29)
+
+# tom
+# ayus 9324531323
+
+# cd
+
+# 60
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 9136712581','hey professional keep going... hard work pays off:)',12,17)
+
+# jenny 9324007247
